e311ee50b02ca8f0f71302fe27fa3d5fdb8e6452
ken chika <116356963+ekenechika@users.noreply.github.com>
2025-03-25 18:16:58 -0700

    Update sums.h

--- sums.h
+++ sums.h
@@ -1,10 +1,15 @@
 #ifndef __sum_h_
 #define __sum_h_
 
-#include <cstdint>
-using std::int64_t;  // Bring int64_t into the global namespace
+// Use the C header for fixed-width integer types
+#include <stdint.h>
+
+// If int64_t isn’t defined, define it using the built-in type
+#ifndef INT64_MAX
+typedef __int64_t int64_t;
+#endif
 
 extern void setup(int64_t N, double A[]);
 extern double sum(int64_t N, double A[]);
 
-#endif
+#endif  
