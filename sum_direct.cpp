b312fb3a4f20d399e6638bf465a425193e664f9d
ken chika <116356963+ekenechika@users.noreply.github.com>
2025-03-25 17:07:07 -0700

    Update sum_direct.cpp

--- sum_direct.cpp
+++ sum_direct.cpp
@@ -1,24 +1,24 @@
-#include <algorithm>
-#include <chrono>
-#include <iomanip>
-#include <iostream>
-#include <random>
-#include <vector>
-#include <string.h>
-
+#include <stdio.h>
 #include "sums.h"
 
-void 
-setup(int64_t N, double A[])
+void setup(int64_t N, double A[])
 {
-   printf(" inside direct_sum problem_setup, N=%lld \n", N);
+    printf("Inside direct_sum problem_setup, N=%lld \n", N);
+    // For direct sum, initialize array elements to 1.0.
+    for (int64_t i = 0; i < N; i++)
+    {
+         A[i] = 1.0;
+    }
 }
 
-double
-sum(int64_t N, double A[])
+double sum(int64_t N, double A[])
 {
-   printf(" inside direct_sum perform_sum, N=%lld \n", N);
-
-   return 0;
+    printf("Inside direct_sum perform_sum, N=%lld \n", N);
+    double total = 0.0;
+    for (int64_t i = 0; i < N; i++)
+    {
+         total += A[i];
+    }
+    return total;
 }
 
